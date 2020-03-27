73961a4448b1e5e2fd45e3301de30345a80f4f9f
Dmytro Didukh <jeriko.outback@gmail.com>
2020-03-27 20:54:25 +0200

    add fontawasome

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className="todo">
+        <div className="todo__sidebar">
+            <ul className="todo__list">
+
+                <li>
+                    <FontAwesomeIcon icon={'stream'}/>
+                    <span>All tasks</span>
+                </li>
+
+            </ul>
+        </div>
+        <div className="todo__tasks">
+
+        </div>
     </div>
-  );
 }
+
 
 export default App;
