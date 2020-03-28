bfa54cb7001035c62706d7f0bc3976c0ed5a719f
Dmytro Didukh <jeriko.outback@gmail.com>
2020-03-28 22:47:22 +0200

    fixup! add fontawasome

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import List from './components/List/index'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function App() {
-    return <div className="todo">
+    return (
+    <div className="todo">
         <div className="todo__sidebar">
-            <ul className="todo__list">
+      <List items={[
+          {
+              icon: <FontAwesomeIcon icon={'stream'}/>,
+              name: 'All tasks',
+              active: true,
 
-                <li>
-                    <FontAwesomeIcon icon={'stream'}/>
-                    <span>All tasks</span>
-                </li>
+          },
+      ]} />
+            <List items={[
+          {
+              color: 'green',
+              name: 'To buy',
+              active: false,
+          },
+          {
+              color: 'pink',
+              name: 'To read',
+              active: false,
+          },
+          {
+              color: 'blue',
+              name: 'Frontend',
+              active: false,
+          },
 
-            </ul>
+      ]} />
         </div>
         <div className="todo__tasks">
-ddd
+
         </div>
     </div>
+    );
 }
 
 
